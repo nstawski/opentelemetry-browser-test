07c02748147d2ec918ef8a055ff1f823629d2871
Nina Stawski <me@ninastawski.com>
2019-10-30 19:25:38 -0700

    Removed unused opentracing

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import * as opentracing from "opentracing";
-
-const tracer = new opentracing.Tracer();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
